a32f88b721fd80a23235a696f12db4eb8a7d8723
SelvaAydinn <selvaaydin972@gmail.com>
2019-07-31 16:33:24 +0300

    click function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from './components/MyComponent' ;
-
+import MyComponent from './components/MyComponent';
+import MyDataComponent from './components/MyDataComponent';
 function App() {
   return (
     <div className="App">
      <MyComponent name="selva">this is my children</MyComponent>
       <MyComponent name="selva">this is your children</MyComponent>
+    <MyDataComponent></MyDataComponent>
+
     </div>
   );
 }
